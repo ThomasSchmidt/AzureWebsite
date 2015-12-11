529bd27efa1a79e1e00b2c17d2d0bdc72e057368
Thomas Schmidt <thomas.schmidt@valtech.dk>
2015-12-11 08:03:09 +0100

    added application insights to project

--- AzureWebsite/App_Start/FilterConfig.cs
+++ AzureWebsite/App_Start/FilterConfig.cs
@@ -1,5 +1,6 @@
 ﻿using System.Web;
 using System.Web.Mvc;
+using AzureWebsite.Domain;
 
 namespace AzureWebsite
 {
@@ -7,7 +8,7 @@ namespace AzureWebsite
 	{
 		public static void RegisterGlobalFilters(GlobalFilterCollection filters)
 		{
-			filters.Add(new HandleErrorAttribute());
+			filters.Add(new ExceptionFilterLogger());
 		}
 	}
 }
--- AzureWebsite/Global.asax.cs
+++ AzureWebsite/Global.asax.cs
@@ -6,6 +6,8 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.Extensibility;
 
 namespace AzureWebsite
 {
@@ -14,10 +16,13 @@ namespace AzureWebsite
 
 	public class MvcApplication : System.Web.HttpApplication
 	{
+		public static TelemetryClient TelemetryClient;
+
 		protected void Application_Start()
 		{
-			AreaRegistration.RegisterAllAreas();
+			TelemetryClient = new TelemetryClient();
 
+			AreaRegistration.RegisterAllAreas();
 			WebApiConfig.Register(GlobalConfiguration.Configuration);
 			FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
 			RouteConfig.RegisterRoutes(RouteTable.Routes);
