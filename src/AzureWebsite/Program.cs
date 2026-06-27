eb788e0a445a612f7c50629e37d73638cdf7824c
Thomas Schmidt <thomas.schmidt@valtech.com>
2026-06-27 12:28:20 +0200

    fix: enable HTTPS redirection in production when HSTS is enabled
    - Add UseHttpsRedirection() middleware in production branch
    - Ensures HSTS is effective for new users arriving via HTTP
    - HSTS alone does not redirect HTTP traffic to HTTPS

--- src/AzureWebsite/Program.cs
+++ src/AzureWebsite/Program.cs
@@ -52,18 +52,14 @@ public class Program
         }
         else
         {
+            app.UseHttpsRedirection();
             app.UseExceptionHandler("/Error");
-            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
             app.UseHsts();
         }
 
         app.UseStaticFiles();
 
         app.UseRouting();
-
-        app.UseAuthentication();
-
-        app.UseAuthorization();
 
         app.UseOutputCache();
 
