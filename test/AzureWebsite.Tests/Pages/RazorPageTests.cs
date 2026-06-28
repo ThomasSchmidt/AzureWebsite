41b7d490117744f6c4620ae2961728d1e3237865
Thomas Schmidt <thomas.schmidt@valtech.com>
2026-06-28 15:01:15 +0200

    test: add IndexModel tests for code coverage improvement
    - Add test for OnGet method call without exception
    - Add test for constructor creating instance without dependencies

--- test/AzureWebsite.Tests/Pages/RazorPageTests.cs
+++ test/AzureWebsite.Tests/Pages/RazorPageTests.cs
@@ -15,4 +15,25 @@ public class RazorPageTests
         // OnGet is void, so we just verify construction and that it doesn't throw
         Assert.NotNull(sut);
     }
+
+    [Fact]
+    public void IndexModel_OnGet_CanBeCalledWithoutException()
+    {
+        var sut = new AzureWebsite.Pages.IndexModel();
+
+        // OnGet has OutputCache attribute but is otherwise empty
+        // Calling it should not throw any exceptions
+        sut.OnGet();
+
+        // If we got here without exception, the test passes
+        Assert.True(true);
+    }
+
+    [Fact]
+    public void IndexModel_Constructor_CreatesInstanceWithoutDependencies()
+    {
+        var sut = new AzureWebsite.Pages.IndexModel();
+
+        Assert.NotNull(sut);
+    }
 }
